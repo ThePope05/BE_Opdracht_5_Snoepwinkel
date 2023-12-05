15fb5445c86c4976217dd3b3a5a2e229f1f77445
ThePope05 <swobben05@gmail.com>
2023-12-05 12:27:45 +0100

    Fixing my own stupid component code

--- app/views/Homepage/index.php
+++ app/views/Homepage/index.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="<?= URLROOT; ?>/css/style.css">
-    <title>Home screen</title>
-</head>
+<?php
+$componentData = [
+    'title' => 'Homepage',
+];
+$this->component('pageHead', $componentData);
+?>
 
 <body>
     <h1><?= $data['title']; ?></h1>
-    <?php $this->component('exampleComponent', ['title' => 'Component']); ?>
 </body>
 
 </html>

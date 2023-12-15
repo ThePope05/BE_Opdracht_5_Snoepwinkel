4e8ba283885bd2cc22cc32b96eeb0e9ba5ab4c5e
ThePope05 <swobben05@gmail.com>
2023-12-15 09:43:35 +0100

    Updated icons

--- app/views/Components/pageHead.php
+++ app/views/Components/pageHead.php
@@ -11,6 +11,7 @@
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
 
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,1,0" />
 
     <link rel="stylesheet" href="/public/css/style.css">
     <title><?= $componentData['title'] ?></title>
